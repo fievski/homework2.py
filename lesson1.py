0f27a264a86d177fc6047503bfd29ac02790c582
fievski <fievski@yandex.ru>
2024-06-04 18:29:39 +0600

    Add files via upload

--- lesson1.py
+++ lesson1.py
@@ -1,5 +1,6 @@
 first_name = 12
 second_name = 1.5
 name_of_course = "Python"
-middle_time = first_name // second_name
-print('Курс:', name_of_course, 'всего задач:', first_name, 'затрачено часов:', second_name,',среднее время выполнения', middle_time, 'часов')
+middle_time =  second_name / first_name
+print(name_of_course,',''всего задач:',first_name, ',затрачено часов',':', second_name ,'среднее время выполнения',':',middle_time,'.')
+    # Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.
