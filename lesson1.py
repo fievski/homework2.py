61f8dcbc73ad8793a3ea8efc814258c65f573768
fievski <fievski@yandex.ru>
2024-06-04 13:44:46 +0600

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,5 @@
+first_name = 12
+second_name = 1.5
+name_of_course = "Python"
+middle_time = first_name // second_name
+print('Курс:', name_of_course, 'всего задач:', first_name, 'затрачено часов:', second_name,',среднее время выполнения', middle_time, 'часов')
